bab1e5a211d15b58af11e1559d51237055184600
Jockey Wilson <jockey_wilson@mac.com>
2019-09-17 14:40:57 -0700

    creates empty data model

---
+++ coder-swag/coder-swag/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Jockey Wilson on 2019-09-17.
+//  Copyright © 2019 Jockey Wilson. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    
+}
