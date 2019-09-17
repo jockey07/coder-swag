bb699a757a7609a7f90d9f9122fdb31e93a6f2b0
Jockey Wilson <jockey_wilson@mac.com>
2019-09-17 15:00:23 -0700

    starting data service

--- coder-swag/coder-swag/Model/Category.swift
+++ coder-swag/coder-swag/Model/Category.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
+//this is a resuseable data struct that can be used over an over again
+//it references the outlets created for the resubale cell
+//simlar can be used to get data from other sources such as databases
+
 struct Category {
+    //private for setting ie no one else can set
+    //public for getting, anyone can get
+    //these are the two items in the cell
+    private(set) public var title : String
+    private(set) public var imageName : String
     
+    //initialize the variables
+    //it says the self ie the vars abouve will be passed what is
+    //the paramaters after the init
+    init(title:String, imageName:String) {
+        self.title = title
+        self.imageName = imageName
+    }
 }
---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Jockey Wilson on 2019-09-17.
+//  Copyright © 2019 Jockey Wilson. All rights reserved.
+//
+
+import Foundation
+
+//this will be used to hold our data for the app
+class DataService {
+    //static used so only one copy of the data for this app
+    //called a singleton
+    static let instance = DataService()
+}
