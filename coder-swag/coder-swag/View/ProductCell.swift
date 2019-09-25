fdae21145d20f3a5da216ae4932fb1021aa4009a
Jockey Wilson <jockey_wilson@mac.com>
2019-09-24 19:49:12 -0700

    prototype designed.outlets linked

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jockey Wilson on 2019-09-24.
+//  Copyright © 2019 Jockey Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productVeiw: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
